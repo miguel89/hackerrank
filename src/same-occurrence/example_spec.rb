ee05417d795476b247cba049344053381ff3fa1b
Miguel Amaral <miguel.a.amaral@gmail.com>
2017-07-20 20:41:24 -0300

    new approach to same occurrence.

--- src/same-occurrence/example_spec.rb
+++ src/same-occurrence/example_spec.rb
@@ -1,5 +1,7 @@
 #!/bin/ruby
-
-
 output = `ruby same-occurrence.rb < input.txt`
+puts output
 puts output == "2\n6\n"
+
+output = `ruby same-occurrence.rb < input_2.txt`
+puts output
--- src/same-occurrence/same-occurrence.rb
+++ src/same-occurrence/same-occurrence.rb
@@ -50,29 +50,93 @@ def max_subarrays_of(a, x)
   c
 end
 
+def count_zero(a, x_indexes, y_indexes)
+  all = (x_indexes + y_indexes).sort
+
+  c = 0
+
+  i = 0
+
+  all.size.times do
+    unless all[i].zero?
+      size = if i.zero?
+               all[i]
+             else
+               all[i] - all[i - 1] - 1
+             end
+      c += max_subarray_count size
+    end
+    i += 1
+  end
+
+  size = a.size - all.last
+  c += max_subarray_count size unless all.last == a.size - 1
+
+  c
+end
+
+def count_subarrays_of_count(a, n, x_indexes, y_indexes)
+  all = (x_indexes + y_indexes).sort
+
+  c = 0
+
+  i = 0
+  j = (n * 2) - 1
+
+  all.size.times do
+    selected = all[i..j]
+
+    if x_indexes.count {|it| selected.include? it} == y_indexes.count {|it| selected.include? it}
+      n_left = if i.zero?
+                 all[i]
+               else
+                 all[i] - all[i - 1] - 1
+               end
+
+      n_right = if j == all.size - 1
+                  if j == a.size - 1
+                    0
+                  else
+                    a.size - all.last
+                  end
+                else
+                  all[j + 1] - all[j] - 1
+                end
+
+      c += 1 + n_left + n_right + if n_left != 0 && n_right != 0
+                                    n_right + n_left - 1
+                                  else
+                                    0
+                                  end
+
+    end
+    i += 1
+    j += 1
+  end
+  c
+end
+
+max_count = max_subarray_count n
 
 q.times do
   x, y = gets.chomp.split(' ').map(&:to_i)
   c = 0
 
   if a.include?(x) && a.include?(y)
-    i = [a.index(x), a.index(y)].min
-    j = [a.rindex(x), a.rindex(y)].compact.max
+    x_indexes = []
+    y_indexes = []
 
-    loop do
-      c += 1 if a[i..j].count(x) == a[i..j].count(y)
+    a.each_with_index do |it, idx|
+      x_indexes << idx if it == x
+      y_indexes << idx if it == y
+    end
 
-      j = [a[0..(j - 1)].rindex(x), a[0..(j - 1)].rindex(y)].compact.max
+    min = [a.count(x), a.count(y)].compact.min
 
-      break if j.nil?
+    c += count_zero(a, x_indexes, y_indexes)
 
-      if j == i
-        i = [a[(i + 1)..-1].index(x), a[(i + 1)..-1].index(y)].compact.min + i + 1
-        j = [a[0..-1].rindex(x), a[0..-1].rindex(y)].max
-      end
-
-      break if i == a.size - 1
-
+    min.times do |it|
+      c += count_subarrays_of_count(a, it + 1, x_indexes, y_indexes)
     end
 
     puts c
@@ -82,7 +146,7 @@ q.times do
   elsif a.include?(y)
     puts max_subarrays_of(a, y)
   else
-    puts max_subarray_count n
+    puts max_count
   end
 
 end
